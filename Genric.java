3f10891e58f6b6b59d030df09166580ca06dd094
arjun98k <arjunkandekar14@gmail.com>
2024-09-11 23:39:24 +0530

    genric wrapper collection framework

--- Genric.java
+++ Genric.java
@@ -1,5 +1,6 @@
 public class Genric {
 	public static void main(String[] args){
 		System.out.println("Hii love cf");
+		System.out.println("how world of corporate world making fun of interns");
 	}
 }
