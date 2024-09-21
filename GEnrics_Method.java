6da6676fa1277379c2f730737e70466d96784f42
arjun98k <arjunkandekar14@gmail.com>
2024-09-21 20:18:15 +0530

    Genrics method

---
+++ GEnrics_Method.java
@@ -0,0 +1,27 @@
+public class GEnrics_Method {
+    // using method overloading
+    // public static void Print(int a){
+    //     System.out.println(a);
+
+    // }
+
+    // public static void Print(String a){
+    //     System.out.println(a);
+    // }
+     
+
+     //Using Genrics
+      public static<M> void Print(M data){
+        System.out.println(data);
+
+      }
+    public static void main(String[] args) {
+        
+        // Print(11);
+        // Print("arjun");
+
+          Print(11);
+          Print("Akash");
+
+    }
+}
