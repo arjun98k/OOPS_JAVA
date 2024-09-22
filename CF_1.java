e4b2820ba47ed1565dde51b6c686aa3ad1f5cfa6
arjun98k <arjunkandekar14@gmail.com>
2024-09-22 23:26:48 +0530

    hii

---
+++ CF_1.java
@@ -0,0 +1,14 @@
+// Java collection frameWork 
+
+//The java colllection framework provides a set of interface and classes 
+// implementation various  data structure and algoirthm. these interface
+// include several method to perform differnt operation on collections
+
+// The Collection interface is the root interface of the java collection
+// framework 
+public class CF_1 {
+	public static void main(String[] args){
+		
+     System.out.println("hii");
+	}
+}
