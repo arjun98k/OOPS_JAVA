37eabc083c1821b9f82dc214ceb8bdc2b846a01d
arjun98k <arjunkandekar14@gmail.com>
2024-09-23 21:12:43 +0530

    Arraylist adding and remove element

--- CF_2.java
+++ CF_2.java
@@ -6,12 +6,17 @@ public class CF_2 {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
 
-        list1 = List.of(11,13,14,15,65);
-        list2 = List.of(24,35,67,55);
-
+        // list1 = List.of(11,13,14,15,65);
+        // list2 = List.of(24,35,67,55);
+          list1.add(11);  list2.add(71);
+                            list2.add(87);
+          list1.add(12);
+          list1.add(13);
+          list1.add(14);  
 
         System.out.println(list1);
-        System.out.println();
+        list1.removeAll(list2);
+        System.out.println(list1);
         
     }
 }
