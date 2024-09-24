c1343db59994dcf2e43171280d7cbbcf1d7ec241
arjun98k <arjunkandekar14@gmail.com>
2024-09-24 20:02:16 +0530

    Arraylist list method

---
+++ CF_3.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CF_3 {
+    public static void main(String[] args) {
+        List<Integer> ls1 = new ArrayList<>();  
+        ls1.add(11); 
+        ls1.add(12);
+        ls1.add(13);
+        ls1.add(14);
+        System.out.println(ls1);
+        System.out.println(ls1.get(0));
+    }
+ 
+}
