c2599b700361d1345f9380ca07f8ca25d97ec281
arjun98k <arjunkandekar14@gmail.com>
2024-09-27 23:59:46 +0530

    hii

---
+++ CF_4.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CF_4 {
+    public static void main(String[] args) {
+        List <String> list1 = new ArrayList<>();
+
+        list1.add("apple");
+        list1.add("mangoes");
+        list1.add("bananas");
+        list1.add("oranges");
+
+        for (int i = 0; i < list1.size(); i++) {
+            System.out.println(list1.get(i));
+            System.err.println("hhi");
+        }
+
+
+    }
+}
